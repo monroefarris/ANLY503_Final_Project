d0973606d8c2cdb63c224476086aa9dcc9289483
Monroe Farris <msf4em@virginia.edu>
2022-03-15 10:29:31 -0400

    minor changes to run with vs code ipykernel

--- avg-accidents-viz.py
+++ avg-accidents-viz.py
@@ -1,10 +1,11 @@
-from typing import final
+#%%
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import datetime as dt
 import os 
 
+#%%
 # storing home dir if needed
 home_dir = os.getcwd()
 
@@ -14,6 +15,7 @@ os.chdir(os.getcwd() + data_dir)
 
 dirs = os.listdir() # getting all years of data
 
+#%%
 final_df = pd.DataFrame()
 for dir in dirs: # looping over all years of data
     if not dir.startswith('.'):
@@ -27,6 +29,7 @@ for dir in dirs: # looping over all years of data
 # grouping by month and counting number of unique id's to get a count of number of accidents for that month
 final_df = final_df.groupby(['Year', 'Month'])['CaseId'].nunique().reset_index().rename(columns = {'CaseId':'NumAccidents'})
 
+#%%
 # subsetting by every 5 years 
 final_df = final_df[(pd.to_numeric(final_df['Year']) % 5 == 0)]
 
@@ -34,9 +37,12 @@ final_df = final_df[(pd.to_numeric(final_df['Year']) % 5 == 0)]
 final_df['Month'] = pd.to_datetime(final_df['Month'], format = "%m")
 final_df['Month'] = final_df['Month'].dt.strftime('%B')
 
+#%%
 sns.lineplot(data=final_df, x="Month", y="NumAccidents", hue="Year")
 plt.xticks(rotation = 45) 
 plt.suptitle('Number of Fatal Accidents per Month from 1975 - 2019')
 #plt.show()
 os.chdir(home_dir)
 plt.savefig('avg-accidents-viz.png')
+
+# %%
